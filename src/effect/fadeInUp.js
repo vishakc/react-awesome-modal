67dae70c7a704c057baa918a65be321f86aa2e17
vishakc <50020557+vishakc@users.noreply.github.com>
2020-06-29 22:45:45 +0530

    Update fadeInUp.js

--- src/effect/fadeInUp.js
+++ src/effect/fadeInUp.js
@@ -1,10 +1,9 @@
 export default {
     'mask': {
-        'position': 'fixed',
+        'position': 'absolute',
         'top': 0,
         'left': 0,
         'width': '100%',
-        'height': '100%',
         'backgroundColor': 'rgba(0, 0, 0, 0.7)',
         'zIndex': 10001
     },
